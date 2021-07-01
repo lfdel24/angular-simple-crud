ad81f656bb7134e238ac2bc9e59b6fffeae58c43
lfdel24 <lfdel24@gmail.com>
2021-06-30 20:27:37 -0500

    Dwlete employe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,17 +15,39 @@ export class AppComponent {
       county: 'Colombia',
     },
     {
-      id: 1,
+      id: 2,
       name: 'Juana Maria',
       county: 'Venezuela',
     },
     {
-      id: 1,
+      id: 3,
       name: 'Oscar Barajas',
       county: 'Mex',
     },
   ];
   selectedEmploye: Employe = new Employe();
+  newEmploye: Employe = new Employe();
 
-  search = '';
+  search: string = '';
+
+  onKey(value: any): void {
+    console.log(value);
+  }
+
+  new(): void {
+    this.newEmploye = new Employe();
+    this.newEmploye.id = this.listEmploye.length + 1;
+    this.newEmploye.name = 'Pabcjkdfsdnkjanda';
+    this.newEmploye.county = 'andkajnsjkadnak';
+    this.listEmploye.push(this.newEmploye);
+    console.log(this.listEmploye.length);
+  }
+
+  delete(id: number): void {
+    this.listEmploye = this.listEmploye.filter((value) => value.id !== id);
+  }
+  clear(): void {
+    this.selectedEmploye = new Employe();
+    this.search = '';
+  }
 }
