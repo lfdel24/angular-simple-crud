a6f298cf4cdd4974036062403d50301ffcb6aed6
lfdel24 <lfdel24@gmail.com>
2021-07-01 15:45:35 -0500

    Separated components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,16 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ListarComponent } from './listar/listar.component';
 import { EmployeService } from './service/employe.service';
+import { SearchComponent } from './search/search.component';
+import { SaveOrEditComponent } from './save-or-edit/save-or-edit.component';
 
 @NgModule({
-  declarations: [AppComponent, ListarComponent],
+  declarations: [
+    AppComponent,
+    ListarComponent,
+    SearchComponent,
+    SaveOrEditComponent,
+  ],
   imports: [BrowserModule, FormsModule],
   providers: [EmployeService],
   bootstrap: [AppComponent],
---
+++ src/app/save-or-edit/save-or-edit.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-save-or-edit',
+  templateUrl: './save-or-edit.component.html',
+  styleUrls: ['./save-or-edit.component.css']
+})
+export class SaveOrEditComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
